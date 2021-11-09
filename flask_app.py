cd2d8acc2dcb2734bdf519308df165d131eeb6b3
Oviya Manoharan <omanohar@uncc.edu>
2021-11-09 09:47:01 -0500

    set up the homepage feature

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,14 @@
 # imports
 import os                 # os is used to get environment variables IP & PORT
 from flask import Flask   # Flask is the web app that we will customize
+from flask import render_template
 
 app = Flask(__name__)     # create an app
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
