c98cc973151e35b0ef18bdf681dba0cbe3c52b01
Oviya Manoharan <omanohar@uncc.edu>
2021-11-12 23:25:05 -0500

    added create post functionality

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,8 @@
 import os                 # os is used to get environment variables IP & PORT
 from flask import Flask   # Flask is the web app that we will customize
 from flask import render_template
+from flask import request
+from flask import redirect, url_for
 from database import db
 from models import Question as Question
 
@@ -29,6 +31,25 @@ def index():
 def get_question(question_id):
     my_question = db.session.query(Question).filter_by(id=question_id).one()
     return render_template('question.html', post=my_question)
+
+
+@app.route('/home/new', methods=['GET', 'POST'])
+def new_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['postText']
+
+        from datetime import date
+        today = date.today()
+
+        today = today.strftime("%m-%d-%Y")
+
+        new_record = Question(title, text, today)
+        db.session.add(new_record)
+        db.session.commit()
+        return redirect(url_for('get_question'))
+    else:
+        return render_template('new_post.html')
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
