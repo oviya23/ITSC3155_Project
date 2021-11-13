a18066258087944ebb126f8495060736eb82c967
Oviya Manoharan <omanohar@uncc.edu>
2021-11-12 19:18:14 -0500

    Added new post functionality and navigation

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,8 @@
 import os                 # os is used to get environment variables IP & PORT
 from flask import Flask   # Flask is the web app that we will customize
 from flask import render_template
+from flask import request
+from flask import redirect, url_for
 from database import db
 from models import Question as Question
 
@@ -18,7 +20,26 @@ with app.app_context():
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
+
+
+@app.route('/index/new', methods=['GET', 'POST'])
+def new_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['postText']
+
+        from datetime import date
+        today = date.today()
+
+        today = today.strftime("%m-%d-%Y")
+
+        new_record = Question(title, text, today)
+        db.session.add(new_record)
+        db.session.commit()
+        return redirect(url_for('get_question'))
+    else:
+        return render_template('new_post.html')
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
