1beeb6fb29f63bb3ebe7f505965ce35fa1f23987
Oviya Manoharan <omanohar@uncc.edu>
2021-11-12 23:02:14 -0500

    fixing for merge pt.2

--- flask_app.py
+++ flask_app.py
@@ -26,7 +26,6 @@ def index():
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
-
 # To see the web page in your web browser, go to the url,
 #   http://127.0.0.1:5000
 
