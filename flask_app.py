0505109b2e191813f3e5301f293391739f5d6a82
cgross15 <cgross15@uncc.edu>
2021-11-12 22:07:32 -0500

    reviewed and edited edit_post function

--- flask_app.py
+++ flask_app.py
@@ -19,6 +19,7 @@ with app.app_context():
 def index():
     return render_template('home.html')
 
+
 @app.route('/home/question/edit/<question_id>', methods=['GET', 'POST'])
 def edit_post(question_id):
     if request.method == 'POST':
@@ -26,7 +27,7 @@ def edit_post(question_id):
         title = request.form['title']
 
         # get post text
-        text = request.form['text']
+        text = request.form['postText']
         post = db.session.query(Question).filter_by(id=question_id).one()
 
         # update question title and text
@@ -37,17 +38,17 @@ def edit_post(question_id):
         db.session.add(post)
         db.session.commit()
 
-        return redirect(url_for('index'))
+        return redirect(url_for('get_question'))
     else:
         # GET request - show new question form to edit the question
         # retrieve user from database
-        # Update this line -----
-        a_user = db.session.query(Account).filter_by(username="test").one()
+        # Fix this line -----
+        # a_user = db.session.query(Account).filter_by(username="test").one()
 
         # retrieve question from database
         my_question = db.session.query(Question).filter_by(id=question_id).one()
 
-        return render_template('edit.html', post=my_question, account=a_user)
+        return render_template('edit.html', post=my_question)
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
