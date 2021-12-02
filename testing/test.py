c4a9967f5dc2293f397a986dd2cd83aaca3759ef
Shriya Kothi <skothi@uncc.edu>
2021-12-02 09:13:38 -0500

    edited test.py

--- testing/test.py
+++ testing/test.py
@@ -3,34 +3,29 @@ import requests
 
 class FlaskTest(unittest.TestCase):
 
-    def test_home(self):
+    def test_loginHTML(self):
         response = requests.get("http://127.0.0.1:5000/login")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
         self.assertEqual('<h1>Welcome to the ROCS Q&A App!</h1>' in response.text, True)
 
-    def test_questions(self):
-        response = requests.get("http://127.0.0.1:5000/home")
+    def test_login(self):
+        response = requests.get("http://127.0.0.1:5000/login")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
-        self.assertEqual('Title' and 'Date' in response.text, True)
+        self.assertEqual('<form method="POST" action="/login">' in response.text, True)
 
-    def test_question(self):
-        response = requests.get("http://127.0.0.1:5000/home/1")
+    def test_registerHTML(self):
+        response = requests.get("http://127.0.0.1:5000/register")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
-        self.assertEqual('First Question' in response.text, True)
+        self.assertEqual('<h1>Welcome to the ROCS Q&A App!</h1>' in response.text, True)
 
-    def test_new(self):
-        response = requests.get("http://127.0.0.1:5000/home/new")
+    def test_register(self):
+        response = requests.get("http://127.0.0.1:5000/register")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
-        self.assertEqual('<form action="new" method="post">' in response.text, True)
-
-    def test_delete(self):
-        response = requests.get('http://127.0.0.1:5000/home/delete/<question_id>')
-        statuscode = response.status_code
-        self.assertEqual(statuscode, 500)
+        self.assertEqual('<form method="POST" action="/register">' in response.text, True)
 
 if __name__ == " __main__":
     unittest.main()
